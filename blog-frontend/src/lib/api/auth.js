a5a37131f894b4531de14345a4a33f23e948fed3
Jaejoon <jaejoon0721@ajou.ac.kr>
2023-05-14 15:46:16 +0900

    0514 프론트 가져가셈

--- blog-frontend/src/lib/api/auth.js
+++ blog-frontend/src/lib/api/auth.js
@@ -2,11 +2,11 @@ import client from './client';
 
 // 로그인
 export const login = ({ username, password }) =>
-  client.post('/api/auth/login', { username, password });
+  client.post('/login', { username, password });
 
 // 회원가입
 export const register = ({ username, password }) =>
-  client.post('/api/auth/register', { username, password });
+  client.post('/register', { username, password });
 
 // 로그인 상태 확인
 export const check = () => client.get('/api/auth/check');
