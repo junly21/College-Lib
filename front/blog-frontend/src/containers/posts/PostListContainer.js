c03814cdf9a77364c2a705ab5d617e96ba06ca7e
junly21 <jaejoon0721@ajou.ac.kr>
2023-06-01 00:40:29 +0900

    포스트 조회 전

--- front/blog-frontend/src/containers/posts/PostListContainer.js
+++ front/blog-frontend/src/containers/posts/PostListContainer.js
@@ -21,7 +21,7 @@ const PostListContainer = () => {
     const tag = searchParams.get('tag');
     // page가 없으면 1을 기본값으로 사용
     const page = parseInt(searchParams.get('page'), 10) || 1;
-    dispatch(listPosts({ tag, username, page }));
+    dispatch(listPosts({ tag, username, page })); //
   }, [dispatch, searchParams, username]);
 
   return (
