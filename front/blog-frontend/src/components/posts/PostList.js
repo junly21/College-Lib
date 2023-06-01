94feec1a59dc96a06780c2a9814efce65406fbe5
junly21 <jaejoon0721@ajou.ac.kr>
2023-06-01 21:39:05 +0900

    포스트 리스트 조회 복구

--- front/blog-frontend/src/components/posts/PostList.js
+++ front/blog-frontend/src/components/posts/PostList.js
@@ -36,72 +36,19 @@ const PostItemBlock = styled.div`
   }
 `;
 
-// const PostItem = ({ post }) => {
-//   const { publishedDate, user, tags, title, body, _id } = post;
-
-//   return (
-//     <PostItemBlock>
-//       <h2>
-//         <Link to={`/@${user.username}/${_id}`}>{title}</Link>
-//       </h2>
-//       <SubInfo
-//         username={user.username}
-//         publishedDate={new Date(publishedDate)}
-//       />
-//       <Tags tags={tags} />
-//       <p>{body}</p>
-//     </PostItemBlock>
-//   );
-// };
-
 const PostItem = ({ post }) => {
-  const { publishedDate, user, tags, title, body, id } = post;
+  const { publishedDate, username, tags, title, body, id } = post;
   return (
     <PostItemBlock>
       <h3>제목</h3>
-      {/* <SubInfo username={'usernametest'} publishedDate={new Date()}></SubInfo>
-      <p> 포스트 내용 일부분 예시 </p> */}
-      <Link to={`/2/${id}`}>{title}</Link>
 
-      {/* 여기 username을 post의 저자로 해야함 */}
-      <SubInfo
-        username={user.username}
-        publishedDate={new Date(publishedDate)}
-      />
+      <Link to={`/2/${id}`}>{title}</Link>
+      <SubInfo username={username} publishedDate={new Date(publishedDate)} />
       <Tags tags={tags} />
       <p>{body}</p>
     </PostItemBlock>
   );
 };
-
-// const PostList = ({ posts, loading, error, showWriteButton }) => {
-//   // 에러 발생 시
-//   if (error) {
-//     return <PostListBlock>에러가 발생했습니다.</PostListBlock>;
-//   }
-
-//   return (
-//     <PostListBlock>
-//       <WritePostButtonWrapper>
-//         {showWriteButton && (
-//           <Button cyan to="/write">
-//             새 글 작성하기
-//           </Button>
-//         )}
-//       </WritePostButtonWrapper>
-//       {/*  로딩 중 아니고, 포스트 배열이 존재할 때만 보여줌 */}
-//       {!loading && posts && (
-//         <div>
-//           {posts.map((post) => (
-//             <PostItem post={post} key={post._id} />
-//           ))}
-//         </div>
-//       )}
-//     </PostListBlock>
-//   );
-// };
-
-// export default PostList;
 
 const PostList = ({ posts, loading, error, showWriteButton }) => {
   if (error) {
